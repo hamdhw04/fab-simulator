e79d92605abbad2077c6d07f896f301ee195f96f
hamdhw04 <hamdhw04@gmail.com>
2026-08-06 22:22:26 +0100

    drift based on exponential distribution, noise based on normal distribution, output of machine is a nonlinear equation of inputs.

--- src/simulation/Main Script.py
+++ src/simulation/Main Script.py
@@ -1,6 +1,6 @@
 import simpy
 import numpy as np
-from basemachine import SensorSpec, BaseMachine
+from basemachine import SensorSpec, BaseMachine , Output
 import random
 import pandas
 import json
@@ -18,44 +18,28 @@ def processing(recipe,id,final_data:dict):
     results_dict = {}
     results_dict[id] = {}
     for machine in recipe:
-        machine_run = env.process(machine.processing(id))
+        machine_run = env.process(machine.running(id,machine.reading,machine.const))
         yield machine_run #hold until the process has completed, then move onto next element
         results_dict[id].update(machine_run.value)
     final_data.update(results_dict)
 
 #Temperature, Growth Rate and Pressure. Will assume atmospheric pressure CVD, using trichlorosilane
 epi_sensors = [
-    SensorSpec(name="temperature", unit="°C", mean=1125, std=25), #for 99.7% of data points to fall between 1050 and 1200 degrees C
-    SensorSpec(name="pressure", unit="Pa", mean=101325, std=30), #shouldn't see strong deviation
-    SensorSpec(name="growth_rate", unit="um/min", mean=3.5, std=0.5), #for 99.7% of data points to fall between 2 and 5 um/min
+    SensorSpec(name="temperature", unit="°C", operating=1125, max=1200, aggression=20, positive=True), #for 99.7% of data points to fall between 1050 and 1200 degrees C
+    SensorSpec(name="pressure", unit="Pa", operating=101325, max=30, aggression=20, positive= False), #shouldn't see strong deviation
 ]
 
-#Temperature, O2 Flow Rate and Time, will assume a target thickness of 100nm. constant temp.
-ox_sensors = [
-    SensorSpec(name="temperature", unit="°C", mean=1100, std=0),
-    SensorSpec(name="o2 flow rate", unit="cm3/min", mean=3000, std=666.67), #for 99.7% of data to fall between 1000-5000 cm3/min
-    SensorSpec(name="time", unit="min", mean=75, std=6.67), #for 99.7% of data to fall between 65-85 minutes
-]
-
-photo_sensors = [
-    SensorSpec(name="exposure intensity", unit="mJ/cm2", mean=125, std=25), #50-200
-    SensorSpec(name="alignment offset", unit="nm", mean=4.75, std=3.167), #0.5-10
-    SensorSpec(name="Relative Humidity", unit="%RH", mean=42.5, std=0.833), #for 99.7% of data to fall between 65-85 minutes
-]
-
+epi_output = Output(optimal = 3, lower_bound = 1, upper_bound = 5)
 env = simpy.Environment()
 
-SIM_RUNTIME = 5000
+SIM_RUNTIME = 1000
 
-epi = BaseMachine(env,"Epi",epi_sensors,30,1.67)
-ox = BaseMachine(env,"Ox",ox_sensors,255,25) #3-5.5 hours including ramp up and ramp down
-photo = BaseMachine(env,"Photo",photo_sensors,5.5,0.5) #4-7 minutes 
+epi = BaseMachine(env,"Epi",epi_sensors,epi_output,30,1.67)
 
-recipes = {'recipe1' : [epi,ox,photo],
-           'recipe2' : [epi,ox,photo,ox]}
+recipes = {'recipe1' : [epi]}
 
 final = {}
-env.process(factory(1000, recipes,final))
+env.process(factory(100, recipes,final))
 
 env.run(until=SIM_RUNTIME)
 print(json.dumps(final,indent= 4))
--- src/simulation/basemachine.py
+++ src/simulation/basemachine.py
@@ -1,6 +1,7 @@
 import simpy
 import numpy as np
 from dataclasses import dataclass, field
+from typing import List
 
 
 env = simpy.Environment()
@@ -9,35 +10,74 @@ env = simpy.Environment()
 class SensorSpec:
     name : str
     unit : str
-    mean : float
-    std : float
+    operating : float
+    max : float
+    aggression : float #between 1-10, 10 being aggressive degradation
+    positive : bool #positive trend or not
+
+@dataclass
+class Output:
+    optimal : float
+    lower_bound : float
+    upper_bound : float
 
 @dataclass
 class BaseMachine:
     env : simpy.Environment
     tool_name : str
     sensors: list[SensorSpec]
+    output_spec : Output
     pr_mean : float
     pr_std : float
+    optimal : float = 0.0
+    lower : float = 0.0
+    upper : float = 0.0
 
-    def processing(self,id):
+    # reading: List[bool] = field(init=False)
+
+    def __post_init__(self):
+        self.reading = [sensor.operating for sensor in self.sensors] #intial readings, will mutate this
+        self.optimal = self.output_spec.optimal
+        self.lower = self.output_spec.lower_bound
+        self.upper = self.output_spec.upper_bound
+
+        self.const = self.optimal / (self.reading[0] + (self.reading[1]**2) + (0.5*(self.reading[0]*self.reading[1]))) #scaling constant to get to correct output scale, alpha = beta = 1, gamma = 0.5 (interaction) in non-linear relationship.
+
+    def running(self,id,reading:list,k:float):
         rng = np.random.default_rng() #each machine has it's own randomness
 
         data = {}
         data[self.tool_name] = []
 
         data[self.tool_name].append(int(self.env.now))
-        print(f"\nLot{id} entered {self.tool_name} at {int(self.env.now)}")
+        # print(f"\nLot{id} entered {self.tool_name} at {int(self.env.now)}")
         process_duration = rng.normal(loc = self.pr_mean, scale = self.pr_std)
         yield self.env.timeout(process_duration)
 
         data[self.tool_name].append(int(self.env.now))
-        print(f"Lot{id} tracked out of {self.tool_name} at {int(self.env.now)}")
+        # print(f"Lot{id} tracked out of {self.tool_name} at {int(self.env.now)}")
 
-        for sensor in self.sensors:
-            reading = rng.normal(loc = sensor.mean, scale = sensor.std)
-            print(f"{sensor.name} = {reading:.1f}{sensor.unit}")
-            data[self.tool_name].append(f"{reading:.1f}{sensor.unit}")
+        for i,sensor in enumerate(self.sensors): #separating into index and items
+
+            if sensor.positive == False:
+                sign = -1
+            else:
+                sign = 1
+
+            drift = sign* np.random.default_rng().exponential(1) #drift modelled as exponential distribution (positive values, no "healing")    
+            noise = np.random.default_rng().normal(loc=0,scale=1) #noise modelled as normal distribution
+            reading[i] = reading[i] + (sensor.aggression * drift) + noise
+
+            print(f"{sensor.name} = {reading[i]:.1f}{sensor.unit}")
+            data[self.tool_name].append(f"{reading[i]:.1f}{sensor.unit}")
+
+        new_output = (k*reading[0]) + (k*(self.reading[1]**2)) + (k*0.5*(self.reading[0]*self.reading[1]))
+        data[self.tool_name].append(new_output)
+
+        if new_output > self.upper or new_output < self.lower:
+            print("Tool failure, repairing")
+            yield self.env.timeout(100)
+            reading = [sensor.operating for sensor in self.sensors]
 
         return data
             
