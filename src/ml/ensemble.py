c2553ebc80cad4e9810f80fbca4b55c7c6f2be0e
hamdhw04 <hamdhw04@gmail.com>
2026-09-08 16:55:37 +0100

    Add bootstrapped GB ensemble for RUL uncertainty quantification

--- src/ml/ensemble.py
+++ src/ml/ensemble.py
@@ -3,43 +3,49 @@ import numpy as np
 from sklearn.ensemble import GradientBoostingRegressor
 import matplotlib.pylab as plt
 
+np.random.seed(42) #global seed
+
 GB_PARAMETERS = {"max_depth": 3, "learning_rate": 0.1, "n_estimators": 100}
+GB_N_BOOTSTRAP_MODELS = 30
 tools = ["Epi","Oxidation","Photo","Etch","Diffusion","Metal Deposition"]
 
-def build_ensemble(training_df, testing_df, n_bootstrapping_models:int, gb_parms: dict =  GB_PARAMETERS) -> list:
+def build_ensemble(training_df, testing_df, n_bootstrapping_models:int = GB_N_BOOTSTRAP_MODELS, gb_parms: dict =  GB_PARAMETERS) -> list:
     models = []
-    n_values = []
+    predictions = []
     final_means = []
     final_std = []
-    avg_std = []
     for i in range(n_bootstrapping_models):
         resamples = training_df.sample(frac=1.0, replace=True)
         model = GradientBoostingRegressor(**gb_parms)
         model.fit(resamples[["Input 1", "Input 2"]], resamples["RUL"])
-        models.append(model)
-    for k in range(int(n_bootstrapping_models/5)+1):
-        constant = 5*k
-        n_values.append(constant)
-        predictions = []
-        for j in range(constant):
-            predictions.append(models[j].predict(testing_df[["Input 1","Input 2"]]))
-        ensemble_metrics = pd.DataFrame(predictions)
-        final_means.append(ensemble_metrics.mean())
-        final_std.append(ensemble_metrics.std())
-    avg_std = [s.mean() for s in final_std]
-    return final_means, final_std, avg_std, n_values
+        models.append(model)        
+    for model in models:
+        predictions.append(model.predict(testing_df[["Input 1","Input 2"]]))
+    ensemble_metrics = pd.DataFrame(predictions)
+    final_means.append(ensemble_metrics.mean())
+    final_std.append(ensemble_metrics.std())
+    return final_means, final_std, 
 
-train_data = pd.read_csv(f"data/processed/Epi_training.csv")
-testing_data = pd.read_csv(f"data/processed/Epi_test.csv")
+comparisons = {}
 
-_,_,avg,n = build_ensemble(train_data,testing_data,50)
+for tool in tools:
 
-plt.figure(figsize=(20, 5))
-plt.plot(n,avg)
-plt.title("Bootstrapping Std Stability with Increasing Bootstrapped GB Regressors")
-plt.xlabel("Number of GB Regressors")
-plt.ylabel("Average Std")
-plt.grid(True)
-plt.ylim(0.15, 0.20)
-plt.savefig("notebooks/convergence_graph.png")
-plt.show()
+    average_std = []
+
+    train_data = pd.read_csv(f"data/processed/{tool}_training.csv")
+    testing_data = pd.read_csv(f"data/processed/{tool}_test.csv")
+
+    means, std = build_ensemble(train_data,testing_data)
+    compare = testing_data
+    means[0].name = "Predicted RUL Mean"
+    std[0].name = "Predicted RUL Std"
+    compare = pd.concat([compare,means[0]],axis=1)
+    compare = pd.concat([compare,std[0]],axis=1)
+    compare_low = compare[compare["RUL"] < 5]
+    compare_high = compare[(compare["RUL"] >= 5) & (compare["RUL"] < 16)]
+    average_std.append(float(compare_low["Predicted RUL Std"].mean()))
+    average_std.append(float(compare_high["Predicted RUL Std"].mean()))
+    comparisons[f"{tool}"] = average_std
+
+print(comparisons)
+
